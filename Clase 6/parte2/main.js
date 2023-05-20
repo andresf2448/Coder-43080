9aa8014edd58161f8fce064b51e5c8b6d4c94fba
andresf2448 <58791994+andresf2448@users.noreply.github.com>
2023-05-20 13:39:01 -0500

    Clase 6

--- Clase 6/parte2/main.js
+++ Clase 6/parte2/main.js
@@ -33,3 +33,86 @@
 //ELIMINAR EL STORAGE
 // localStorage.removeItem("nombre"); //elimina por clave
 // localStorage.clear(); // elimina todo el localStorage
+
+// let objeto = {
+//   nombre: "andres",
+//   edad: 26,
+// };
+
+// localStorage.setItem("objeto", JSON.stringify(objeto));
+
+// let arreglo = [1, 2, 3, 4, 5];
+// localStorage.setItem("arreglo", JSON.stringify(arreglo));
+
+// let arreglo = localStorage.getItem("arreglo");
+// let objeto = localStorage.getItem("objeto");
+
+// console.log(JSON.parse(arreglo));
+// console.log(JSON.parse(objeto));
+
+// const productos = [
+//   { id: 1, nombre: "camisa", precio: 1000 },
+//   { id: 2, nombre: "zapato", precio: 1000 },
+//   { id: 3, nombre: "media", precio: 1000 },
+//   { id: 4, nombre: "gorra", precio: 1000 },
+// ];
+
+// const guardar = (clave, valor) => {
+//   localStorage.setItem(clave, valor);
+// };
+
+// productos.forEach(item => {
+//   guardar(item.id, JSON.stringify(item))
+// })
+
+// localStorage.setItem("carrito", JSON.stringify(productos));
+
+// let usuario;
+// let usuarioStorage = sessionStorage.getItem("usuario");
+
+// if (usuarioStorage) {
+//   usuario = usuarioStorage;
+//   alert(`Bienvendi@ ${usuario} nuevamente`);
+// } else {
+//   usuario = prompt("Ingrese el usuario");
+//   alert(`Bienvenid@ ${usuario} es tu primera vez`);
+//   sessionStorage.setItem("usuario", usuario);
+// }
+
+// const productos = [
+//   { id: 1, nombre: "camisa", precio: 1000 },
+//   { id: 2, nombre: "zapato", precio: 1000 },
+//   { id: 3, nombre: "media", precio: 1000 },
+//   { id: 4, nombre: "gorra", precio: 1000 },
+// ];
+
+// localStorage.setItem("carrito", JSON.stringify(productos));
+
+let carrito = [];
+let boton = document.getElementById("eliminar");
+let carritoStorage = localStorage.getItem("carrito");
+
+if (carritoStorage) {
+  carrito = JSON.parse(carritoStorage);
+} else {
+  let div = document.createElement("div");
+  div.innerHTML = "El carrito eestá vacío";
+  document.body.append(div);
+}
+
+carrito.forEach((item) => {
+  let div = document.createElement("div");
+  div.innerHTML = `
+    <h2>ID: ${item.id}</h2>
+    <p>Nombre: ${item.nombre}</p>
+    <b>$${item.precio}</b>
+    <hr />
+  `;
+  document.body.append(div);
+});
+
+boton.addEventListener("click", () => {
+  localStorage.clear();
+  alert("carrito eliminado");
+  location.reload();
+});
