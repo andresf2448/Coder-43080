86bb1375f399962e488e06ae80f1fb22941bb65a
andresf2448 <58791994+andresf2448@users.noreply.github.com>
2023-05-20 10:06:59 -0500

    Clase 6

---
+++ Clase 6/main.js
@@ -0,0 +1,20 @@
+let boton = document.getElementById("boton");
+
+// const respuesta = () => {
+//   alert("click");
+// }
+
+//Primera forma de agregar un evento
+// boton.addEventListener("click", respuesta);
+
+//Segunda forma de agregar un evento
+// boton.onclick = respuesta;
+
+// let evento = prompt("Ingrese el evento");
+// boton.addEventListener(evento, respuesta);
+
+// const respuesta = (nombre) => {
+  // alert(`Hola ${nombre}`);
+// };
+
+// boton.addEventListener("click", () => respuesta("andres"));
