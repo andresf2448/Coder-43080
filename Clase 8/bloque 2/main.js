535bbb1655e6884fe2165d0e5f946819c6889082
andresf2448 <58791994+andresf2448@users.noreply.github.com>
2023-06-03 13:09:24 -0500

    Clase 8

--- Clase 8/bloque 2/main.js
+++ Clase 8/bloque 2/main.js
@@ -67,13 +67,62 @@
 //   console.log("Hola");
 // }, 1000);
 
-let counter = 0;
-const interval = setInterval(() => {
-  counter++;
-  console.log("Counter: ", counter);
+// let counter = 0;
+// const interval = setInterval(() => {
+//   counter++;
+//   console.log("Counter: ", counter);
 
-  if (counter >= 5) {
-    clearInterval(interval);
-    console.log("Se removió el intervalo");
-  }
-}, 1000);
+//   if (counter >= 5) {
+//     clearInterval(interval);
+//     console.log("Se removió el intervalo");
+//   }
+// }, 1000);
+
+//PROMESAS
+// new Promise((resolve, reject) => {
+//   //codigo de la promesa
+// })
+
+// const futuro = (valor) => {
+//   return new Promise((resolve, reject) => {
+//     valor ? resolve(["andres", "juan"]) : reject("Promesa rechazada");
+//   });
+// };
+
+// futuro(true)
+//   .then((response) => console.log(response))
+//   .catch((error) => console.log(error))
+//   .finally(() => console.log("proceso terminado"));
+
+/////////////////////////////////////////////
+const DB = [
+  { id: 1, nombre: "camisa", precio: 1000 },
+  { id: 2, nombre: "zapato", precio: 1000 },
+  { id: 3, nombre: "gorra", precio: 1000 },
+  { id: 4, nombre: "media", precio: 1000 },
+];
+
+const traerProductos = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(DB);
+    }, 3000);
+  });
+};
+////////////////////////////////////////////
+
+traerProductos()
+  .then((response) => {
+    response.forEach((producto) => {
+      let div = document.createElement("div");
+      div.innerHTML = `
+        <h2>Id: ${producto.id}</h2>
+        <p>Nombre: ${producto.nombre}</p>
+        <b>$${producto.precio}</b>
+        <hr />
+      `;
+
+      document.body.append(div);
+    });
+  })
+  .catch((error) => console.log(error));
